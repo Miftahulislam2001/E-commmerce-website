8c24ac6b7ec126e2e9b62f0d289235dc0eb51b92
miftahullislam <miftahullislamsohan@gmail.com>
2024-01-31 10:54:06 +0600

    Navbar section added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import { Search } from '@mui/icons-material';
+import { AddShoppingCartOutlined, Search } from '@mui/icons-material';
+import { Badge } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className='navbar h-[60px] shadow-md rounded-md relative z-10'>
             <div className='wrapper px-5 py-2 flex justify-between items-center'>
+                {/* Left side */}
                 <div className='flex flex-1 items-center'>
                     <div className='language cursor-pointer text-[1rem]'>En</div>
                     <div className='searchInput border-2 border-lightgray border-solid ml-2 px-2 py-1 rounded focus-within:border-purple-300 transition-all'>
@@ -12,8 +15,21 @@ const Navbar = () => {
                         <Search className='cursor-pointer '/>
                     </div>
                 </div>
-                <div>2</div>
-                <div>3</div>
+
+                {/* center logo */}
+                <div className='flex flex-1 items-center justify-center'>
+                    <h2 className='font-semibold text-xl font-serif'>Winter Shops</h2>
+                </div>
+
+                {/* Right side */}
+                <div className='flex flex-1 items-center justify-end'>
+                    <Link className='font-semibold text-lg ml-3' to = "/register">Register</Link>
+                    <Link className='font-semibold text-lg ml-3' to = "/signin">Sign in</Link>
+                    <Link className='font-semibold text-lg ml-3' to = "/signin">
+                        <Badge color='primary' badgeContent={2}><AddShoppingCartOutlined/></Badge>
+                    </Link>
+                                     
+                </div>
             </div>
         </nav>
     );
