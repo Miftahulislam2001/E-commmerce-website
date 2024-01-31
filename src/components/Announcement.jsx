4fdfabde1ef0cb6d4f7c717871751c65ecbab46e
miftahullislam <miftahullislamsohan@gmail.com>
2024-01-31 08:44:07 +0600

    Ecommerce project init with tailwind

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Announcement = () => {
+    return (
+        <div>
+            <h2>Hurry up, it is 40% off now</h2>
+        </div>
+    );
+};
+
+export default Announcement;
